1aa7a3bbbc6ac28ee921dd10a9a55f79be49b41a
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-08 23:02:42 +0700

    service: added service & impl.

---
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/service/FeedbackService.java
@@ -0,0 +1,7 @@
+package dev.bengi.feedbackservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface FeedbackService {
+}
---
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/service/impl/FeedbackServiceImpl.java
@@ -0,0 +1,10 @@
+package dev.bengi.feedbackservice.service.impl;
+
+import dev.bengi.feedbackservice.service.FeedbackService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class FeedbackServiceImpl implements FeedbackService {
+
+    
+}
