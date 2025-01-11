6ffa06f52eab7f26cc30f4e4b874fb7ccd9f27fb
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-11 21:27:28 +0700

    Update ProjectRepository.java

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/ProjectRepository.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/ProjectRepository.java
@@ -2,8 +2,12 @@ package dev.bengi.feedbackservice.repository;
 
 import dev.bengi.feedbackservice.domain.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Project p WHERE p.name = :name")
+    boolean existsByNameIgnoreCase(String name);
 }
