980b7e149f9cd401ab5478af22259f09092b110e
Parking <122780034+kongphop1209@users.noreply.github.com>
2025-01-11 22:25:01 +0700

    Update page.tsx

--- frontend/src/app/(pages)/dashboard_admin/page.tsx
+++ frontend/src/app/(pages)/dashboard_admin/page.tsx
@@ -9,8 +9,8 @@ const Page = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const renderContent = () => {
-    const Component = lazy(() =>
-      import(`@/app/(pages)/dashboard_admin/section/${selectedComponent}`)
+    const Component = lazy(
+      () => import(`@/app/(pages)/dashboard_admin/section/${selectedComponent}`)
     );
 
     return (
@@ -31,7 +31,11 @@ const Page = () => {
   return (
     <div className="flex flex-row bg-gray-50">
       <SidebarAdmin onOptionSelect={handleOptionSelect} />
-      <div className="flex-1 p-5 ">
+      <div className="flex-1 p-5 flex gap-5 flex-col">
+        <div className="border-2 border-red-600 p-6 flex flex-row justify-between rounded-md">
+          <div>a</div>
+          <div>b</div>
+        </div>
         <div className="p-6 border-2 border-red-600 rounded-md w-full h-full">
           {isLoading ? <LoadingScreen /> : renderContent()}
         </div>
