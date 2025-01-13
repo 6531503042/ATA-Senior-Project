ed4a74cac5194973703129b1ab7b065772a75584
Parking <122780034+kongphop1209@users.noreply.github.com>
2025-01-13 12:09:45 +0700

    fixc

--- frontend/src/app/(pages)/dashboard_admin/section/usermanagement.tsx
+++ frontend/src/app/(pages)/dashboard_admin/section/usermanagement.tsx
@@ -8,26 +8,24 @@ const usermanagement = () => {
         {/* Container*/}
         <div>
           {/* Sort */}
-          <ul className="inline-flex flex-row text-base font-bold w-[1520px] items-center p-3 text-center border border-slate-200">
-            <li className="w-1/6 border-r">Name</li>
-            <li className="w-1/6 border-r">Email</li>
-            <li className="w-1/6 border-r">Role</li>
-            <li className="w-1/6 border-r">Status</li>
-            <li className="w-1/6 border-r">Last Login</li>
-            <li className="w-1/6">Actions</li>
+          <ul className="inline-flex flex-row text-base font-bold w-[1520px] h-auto items-center text-center border border-b-0 border-slate-200">
+            <li className="w-1/6 border-r p-3">Name</li>
+            <li className="w-1/6 border-r p-3">Email</li>
+            <li className="w-1/6 border-r p-3">Role</li>
+            <li className="w-1/6 border-r p-3">Status</li>
+            <li className="w-1/6 border-r p-3">Last Login</li>
+            <li className="w-1/6 border-r-0 p-3">Actions</li>
           </ul>
         </div>
-        <div>
           {/*Column DATA*/}
-          <ul className="inline-flex flex-row text-base font-bold w-[1520px] items-center p-3 text-center border border-slate-200">
-            <li className="w-1/6 border-r">{/* Name */}</li>
-            <li className="w-1/6 border-r">{/* Email */}</li>
-            <li className="w-1/6 border-r">{/* Role */}</li>
-            <li className="w-1/6 border-r">{/* Status */}</li>
-            <li className="w-1/6 border-r">{/* Last Login */}</li>
-            <li className="w-1/6">{/* Actions */}</li>
+          <ul className="inline-flex flex-row text-base font-medium w-[1520px] h-auto items-center text-start border border-slate-200">
+            <li className="w-1/6 border-r p-3">{/* Name */}mocking name</li>
+            <li className="w-1/6 border-r p-3">{/* Email */}mocking email</li>
+            <li className="w-1/6 border-r p-3">{/* Role */}mocking role</li>
+            <li className="w-1/6 border-r p-3">{/* Status */}mocking status</li>
+            <li className="w-1/6 border-r p-3">{/* Last Login */}mocking date</li>
+            <li className="w-1/6 border-r-0 p-3">{/* Actions */}mocking feature</li>
           </ul>
-        </div>
       </div>
     </div>
   );
