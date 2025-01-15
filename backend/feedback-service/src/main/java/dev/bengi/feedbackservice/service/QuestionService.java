bc2ff0b15bf442285cb2ef14721febde5280caac
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-15 15:23:10 +0700

    Backend: updated service interface;

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/service/QuestionService.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/service/QuestionService.java
@@ -2,19 +2,33 @@ package dev.bengi.feedbackservice.service;
 
 import dev.bengi.feedbackservice.domain.model.Question;
 import dev.bengi.feedbackservice.domain.payload.request.CreateQuestionRequest;
+import dev.bengi.feedbackservice.domain.payload.request.CreateQuestionSetRequest;
 import dev.bengi.feedbackservice.domain.payload.response.QuestionResponse;
+import dev.bengi.feedbackservice.domain.payload.response.QuestionSetResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface QuestionService {
     Question createQuestion(CreateQuestionRequest request);
 
+    List<QuestionSetResponse> createQuestionSet(CreateQuestionSetRequest request);
+
     @Transactional
     QuestionResponse updateQuestion(Long id, CreateQuestionRequest request);
 
+    QuestionSetResponse updatedQuestionSet(Long id, CreateQuestionSetRequest request);
+
     void deleteQuestion(Long id);
 
+    void deleteQuestionSet(Long id);
+
     Page<Question> getAllQuestions(int page, int size);
+
+    Page<QuestionSetResponse> getAllQuestionSet(int page, int size);
+
+    QuestionSetResponse getQuestionSetId(Long id);
 
     Question getQuestionById(Long id);
 }
