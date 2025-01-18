593bb70d4ab589ab9cb50b05d7c6626c72ae7966
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-18 22:16:09 +0700

    Backend: add security JwtTokenFilter for foreign-client.

---
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/security/JwtTokenFilter.java
@@ -0,0 +1,28 @@
+package dev.bengi.feedbackservice.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+    /**
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        
+    }
+}
