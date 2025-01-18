81e6ce128e9dfb6427139ee3819100b63820502e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-18 22:15:42 +0700

    Backend: added controller for employee.

---
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/controller/employee/SubmitController.java
@@ -0,0 +1,28 @@
+package dev.bengi.feedbackservice.controller.employee;
+
+import dev.bengi.feedbackservice.domain.model.Feedback;
+import dev.bengi.feedbackservice.domain.payload.request.SubmitFeedbackRequest;
+import dev.bengi.feedbackservice.service.FeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/employee/submit")
+@RequiredArgsConstructor
+public class SubmitController {
+
+    private final FeedbackService feedbackService;
+
+
+    @PostMapping("/submit")
+    public ResponseEntity<Feedback> submitFeedback(
+            @RequestBody SubmitFeedbackRequest request
+    ) {
+        var feedbackResponse = feedbackService.submitFeedback(request.getUserId(), request);
+        return ResponseEntity.ok(feedbackResponse);
+    }
+}
