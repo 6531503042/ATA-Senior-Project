f84bd2ff7e8b401a15d4240d6f2c20807e19cd0b
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-21 19:56:48 +0700

    Backend: added Jwt for security.

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/security/JwtTokenFilter.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/security/JwtTokenFilter.java
@@ -1,16 +1,27 @@
 package dev.bengi.feedbackservice.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtPro;
     /**
      * @param request
      * @param response
@@ -23,6 +34,44 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        
+
+        String token = extractToken(request);
+        try {
+            if (token != null && JwtProvider.validateToken(token)) {
+                Authentication auth = jwtPro.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+                // Set JWT token in request attribute
+                request.setAttribute("JWT_TOKEN", token);
+
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid JWT token");
+            }
+        } catch (ExpiredJwtException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Expired JWT token");
+        } catch (MalformedJwtException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid JWT token");
+        }
+
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer")) {
+            return authHeader.replace("Bearer", "");
+        }
+        return null;
+    }
+
+    public static String getTokenFromRequest() {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (servletRequestAttributes != null) {
+            HttpServletRequest request = servletRequestAttributes.getRequest();
+            return (String) request.getAttribute("JWT_TOKEN");
+        }
+
+        return null;
     }
 }
