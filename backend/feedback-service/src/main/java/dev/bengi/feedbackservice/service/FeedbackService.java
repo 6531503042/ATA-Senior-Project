364e4e1379b99f912b7c5ba6bfd1c086bed4ec19
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-23 23:19:31 +0700

    Backend: updated service interface for (Feedback-service)

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/service/FeedbackService.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/service/FeedbackService.java
@@ -3,6 +3,7 @@ package dev.bengi.feedbackservice.service;
 import dev.bengi.feedbackservice.domain.model.Feedback;
 import dev.bengi.feedbackservice.domain.payload.request.CreateFeedbackRequest;
 import dev.bengi.feedbackservice.domain.payload.request.SubmitFeedbackRequest;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +12,15 @@ public interface FeedbackService {
 
     Page<Feedback> getAllFeedbacks(int page, int size);
 
+    Page<Feedback> getAllFeedbacks(int page, int size, String status);
+
     @Transactional(readOnly = true)
     Feedback getFeedbackById(Long id);
+
+    Page<Feedback> getFeedbacksByUser(Long userId, int page, int size);
+
+    @Cacheable(value = "feedbackCache", key = "#projectId")
+    Page<Feedback> getFeedbackByProject(Long projectId, int page, int size);
 
     Feedback submitFeedback(Long userId, SubmitFeedbackRequest request);
 }
