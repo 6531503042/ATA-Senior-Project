d1766ea9dbddb7aa69efa89733cd13c4b822affc
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-01-26 21:39:46 +0700

    user-service: updated request of payload.

---
+++ backend/user-service/src/main/java/dev/bengi/userservice/domain/payload/request/EmailDetails.java
@@ -0,0 +1,17 @@
+package dev.bengi.userservice.domain.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDetails {
+    private String recipient;
+    private String subject;
+    private String msgBody;
+    private String attachment;
+}
