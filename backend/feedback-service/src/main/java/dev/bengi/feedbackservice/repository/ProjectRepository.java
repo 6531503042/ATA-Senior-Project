2cabe21bd62a332d98cf0d55d10d6d831169ffe5
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-02-04 22:30:36 +0700

    Update ProjectRepository.java

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/ProjectRepository.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/ProjectRepository.java
@@ -3,7 +3,10 @@ package dev.bengi.feedbackservice.repository;
 import dev.bengi.feedbackservice.domain.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
@@ -13,4 +16,7 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("SELECT p FROM Project p WHERE p.id = :id")
     Project findAll(Long id);
+
+    @Query("SELECT p FROM Project p JOIN p.memberIds m WHERE m = :memberId")
+    List<Project> findByMemberIdsContaining(@Param("memberId") Long memberId);
 }
