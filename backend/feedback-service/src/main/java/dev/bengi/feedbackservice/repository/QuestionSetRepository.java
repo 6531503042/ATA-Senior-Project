158ff759a10b78b429460f98e77699d39a2d2d91
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-02-15 23:06:05 +0700

    Remove unused files.

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/QuestionSetRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.bengi.feedbackservice.repository;
-
-import dev.bengi.feedbackservice.domain.model.QuestionSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionSetRepository extends JpaRepository<QuestionSet, Long> {
-}
