5a81835e40085492910ba608976e9880d0e953a9
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2025-02-15 23:15:11 +0700

    feedback: updated repository.

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/ProjectRepository.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/repository/ProjectRepository.java
@@ -3,10 +3,9 @@ package dev.bengi.feedbackservice.repository;
 import dev.bengi.feedbackservice.domain.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
@@ -14,9 +13,9 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Project p WHERE p.name = :name")
     boolean existsByNameIgnoreCase(String name);
 
-    @Query("SELECT p FROM Project p WHERE p.id = :id")
-    Project findAll(Long id);
-
-    @Query("SELECT p FROM Project p JOIN p.memberIds m WHERE m = :memberId")
-    List<Project> findByMemberIdsContaining(@Param("memberId") Long memberId);
+    @Query("SELECT COUNT(p) FROM Project p WHERE p.projectEndDate > CURRENT_TIMESTAMP")
+    Long countActiveProjects();
+    
+    @Query("SELECT COUNT(p) FROM Project p WHERE p.projectEndDate <= CURRENT_TIMESTAMP")
+    Long countCompletedProjects();
 }
