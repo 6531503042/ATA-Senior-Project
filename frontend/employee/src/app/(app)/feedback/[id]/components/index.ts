fec88d3fa2fe4223fb8ebfd123b422de038ff958
parking <6531503008@lamduan.mfu.ac.th>
2025-09-08 00:39:26 +0700

    update FeedbackForm
    add popup when complete Submitting the form

--- frontend/employee/src/app/(app)/feedback/[id]/components/index.ts
+++ frontend/employee/src/app/(app)/feedback/[id]/components/index.ts
@@ -3,6 +3,7 @@ export { FeedbackNavigation } from "./FeedbackNavigation";
 export { FeedbackOverallComments } from "./FeedbackOverallComments";
 export { FeedbackProgress } from "./FeedbackProgress";
 export { default as FeedbackStepper } from "./FeedbackStepper";
-export { default as QuestionCard } from "./QuestionCard";
 export { default as SatisfactionDashboard } from "./SatisfactionDashboard";
 export { default as SentimentIndicator } from "./SentimentIndicator";
+export { default as QuestionCard } from "./QuestionCard";
+export { default as CongratsModal } from "./CongratsModal";
